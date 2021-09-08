f2967fca7eaa7ef1af883dc1110b2523d48bffbd
Thor Anderson <ta.anderson@gmail.com>
2021-09-08 09:04:26 -0600

    Added SenatorContainer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import './App.css'
-import './components/SenatorCard'
-import { Box, Grid } from '@material-ui/core'
-import SenatorCard from './components/SenatorCard'
-import { senators } from './data/senate'
+import SenatorContainer from './components/SenatorContainer'
 
 const App = () => {
   return (
     <div className="App">
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          backgroundColor: '#999999',
-        }}
-      >
-        <Grid container item lg={9} spacing={3} xs={12}>
-          return
-          
-            {senators.map((senator) => {
-              return <Grid item md={4} sm={6} xs={12}>
-                <SenatorCard
-                  firstName={senator.first_name}
-                  lastName={senator.last_name}
-                  id={senator.govtrack_id}
-                />
-                </Grid>
-            })}
-          
-        </Grid>
-      </Box>
+      <SenatorContainer/>
     </div>
   )
 }
