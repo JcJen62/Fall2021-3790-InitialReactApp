167991db742e554e31ce77b16267654162c93206
Thor Anderson <ta.anderson@gmail.com>
2021-09-15 09:56:56 -0600

    Messing with styles

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -1,18 +1,10 @@
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
+//import Box from '@mui/material/Box'
 import CardActions from '@mui/material/CardActions'
 import Button from '@mui/material/Button'
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
 const SenatorCard = (senator) => {
   /* const { firstName, lastName, id } = props */
@@ -20,17 +12,19 @@ const SenatorCard = (senator) => {
   return (
     <Card key={senator.id} className='card' sx={{
       width: 168,
-      
-      bgcolor: 'primary.dark',
+      height: 120,
+      p: 0,
+      m: 1,
+      border: 1,
+      borderColor: '#f00',
       '&:hover': {
-        backgroundColor: '#bbb',
         opacity: [0.9, 0.8, 0.7],
       },
     }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="success.dark" gutterBottom>{senator.first_name} {senator.last_name}</Typography>
-        <Typography variant="h6" component="div">
-          be{bull}
+        <Typography sx={{ fontSize: 11 }}>
+          {senator.leadership_role}
         </Typography>
       </CardContent>
     <CardActions>
