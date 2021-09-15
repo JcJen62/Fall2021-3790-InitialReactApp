cd9568b83b7d70ae5fe8273521aff4cd16f4c832
Thor Anderson <ta.anderson@gmail.com>
2021-09-15 11:27:48 -0600

    Added modal controlled by parent

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -3,32 +3,48 @@ import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 //import Box from '@mui/material/Box'
 import CardActions from '@mui/material/CardActions'
-import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity'
 
+const colorVar = '#ff0000'
 
-const SenatorCard = (senator) => {
-  /* const { firstName, lastName, id } = props */
+const SenatorCard = (props) => {
+  
+  const [openModal, closeModal] = props.modalFunctions
+
+  const handleInfoClick = () => {
+    console.log('Clicked!!')
+    openModal()
+  }
 
   return (
-    <Card key={senator.id} className='card' sx={{
-      width: 168,
-      height: 120,
-      p: 0,
-      m: 1,
-      border: 1,
-      borderColor: '#f00',
-      '&:hover': {
-        opacity: [0.9, 0.8, 0.7],
-      },
-    }}>
+    <Card
+      className='card'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: 168,
+        height: 120,
+        p: 0,
+        m: 1,
+        border: 1,
+        borderColor: {colorVar},
+        '&:hover': {
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="success.dark" gutterBottom>{senator.first_name} {senator.last_name}</Typography>
-        <Typography sx={{ fontSize: 11 }}>
-          {senator.leadership_role}
+        <Typography sx={{ fontSize: 14 }} color='success.dark' gutterBottom>
+          {props.senator.first_name} {props.senator.last_name}
         </Typography>
+        <Typography sx={{ fontSize: 11 }}>{props.senator.leadership_role}</Typography>
       </CardContent>
-    <CardActions>
-        <Button>Learn More</Button>
+      <CardActions>
+        <IconButton sx={{ p: 0 }} onClick={handleInfoClick}>
+          <PermIdentityIcon />
+        </IconButton>
       </CardActions>
     </Card>
   )
