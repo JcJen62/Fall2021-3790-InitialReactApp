a81a6241ba1d1feaa419bdd6b5d6a1f447250380
Thor Anderson <ta.anderson@gmail.com>
2021-09-22 15:43:15 -0600

    Added more state for favorites

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -1,16 +1,25 @@
+import * as React from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import CardActions from '@mui/material/CardActions'
 import IconButton from '@mui/material/IconButton'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 const SenatorCard = (props) => {
-  /* const { firstName, lastName, id } = props */
+  const [favorite, setFavorite] = React.useState(false)
+
+  const partyColor = props.senator.party === 'D' ? '#0000ff' : '#e71d36'
 
   const handleInfoClick = () => {
-    console.log('Thanks for clicking!')
     props.modalFunction()
+  }
+
+  const handleFavoriteClick = () => {
+    //console.log(props.senator.id)
+    setFavorite(!favorite)
+    props.addToFavoritesFunction(props.senator.id)
   }
 
   return (
@@ -23,6 +32,8 @@ const SenatorCard = (props) => {
         width: 168,
         height: 120,
         m: 1,
+        border: 5,
+        borderColor: partyColor
 /*         '&:hover': {
           opacity: [0.9, 0.8, 0.7],
         }, */
@@ -37,6 +48,9 @@ const SenatorCard = (props) => {
       <CardActions>
         <IconButton sx={{ p: 0 }} onClick={handleInfoClick}>
           <PermIdentityIcon />
+        </IconButton>
+        <IconButton sx={{ p: 0, m: 0 }} onClick={handleFavoriteClick}>
+          <FavoriteIcon sx={{ color: favorite ? '#F00' : '#000'}}/>
         </IconButton>
       </CardActions>
     </Card>
--- src/components/SenatorContainer.js
+++ src/components/SenatorContainer.js
@@ -17,27 +17,44 @@ const style = {
   p: 4,
 }
 
+const initialIDs = ['23432', '23432']
+
 const SenatorContainer = () => {
-const [open, setOpen] = React.useState(false)
-const handleOpen = () => setOpen(true)
-const handleClose = () => setOpen(false)
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const [favorites, setFavorites] = React.useState(initialIDs)
+
+  const addToFavorites = (senatorID) => {
+    console.log(`${senatorID} added to favorites`)
+    setFavorites((prevState) => {
+      return [...prevState, senatorID]
+    }) // need to sort out the toggling to add and remove senator ids
+  }
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexWrap: 'wrap'
-    }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+      }}
+    >
+      {favorites.map((senatorID) => {
+        return <p>{senatorID}</p>
+      })}
       {senators.map((senator) => {
         return (
-          <SenatorCard modalFunction={handleOpen} senator={{...senator}}
+          <SenatorCard
+            key={senator.id}
+            addToFavoritesFunction={addToFavorites}
+            modalFunction={handleOpen}
+            senator={{ ...senator }}
           />
         )
       })}
       <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
-          <Typography variant="h6">
-            Senator Information
-          </Typography>
+          <Typography variant="h6">Senator Information</Typography>
         </Box>
       </Modal>
     </Box>
