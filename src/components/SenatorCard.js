f5d49e253b7e426add349ecee4c560674b9cc22f
Thor Anderson <ta.anderson@gmail.com>
2021-10-10 20:32:32 -0600

    Caught bad image, improved layout

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -7,10 +7,10 @@ import CardActions from '@mui/material/CardActions'
 import IconButton from '@mui/material/IconButton'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import imagePlaceholder from './palpatine.png'
 
 const SenatorCard = (props) => {
   const [favorite, setFavorite] = React.useState(false)
-
   const partyColor = props.senator.party === 'D' ? '#0000ff' : '#e71d36'
 
   const handleInfoClick = () => {
@@ -23,6 +23,12 @@ const SenatorCard = (props) => {
     props.addToFavoritesFunction(props.senator)
   }
 
+  const handleImageError = (e) => {
+    console.log('Got a bad image')
+    e.target.onerror = null;
+    e.target.src = imagePlaceholder
+  }
+
   return (
     <Card
       className="card"
@@ -30,23 +36,22 @@ const SenatorCard = (props) => {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
-        width: 168,
-        height: 120,
         m: 1,
-        border: 5,
-        borderColor: partyColor
+        border: 2,
+        borderColor: partyColor,
+        width: 120,
 /*         '&:hover': {
           opacity: [0.9, 0.8, 0.7],
         }, */
       }}
     >
       <CardMedia component='img' alt='Congress member' height='200'
-      image={`https://www.govtrack.us/static/legislator-photos/${props.senator.govtrack_id}-200px.jpeg`}/>
-      <CardContent>
+      image={`https://www.govtrack.us/static/legislator-photos/${props.senator.govtrack_id}-100px.jpeg`}
+      onError={handleImageError}/>
+      <CardContent sx={{ p: '4px' }}>
         <Typography sx={{ fontSize: 14 }} color="success.dark" gutterBottom>
           {props.senator.first_name} {props.senator.last_name}
         </Typography>
-        <Typography variant="h6" component="div"></Typography>
       </CardContent>
       <CardActions>
         <IconButton sx={{ p: 0 }} onClick={handleInfoClick}>
