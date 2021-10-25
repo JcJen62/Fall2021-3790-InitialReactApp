1d242c581eb3e7251d36923f264044e0e4298478
Thor Anderson <ta.anderson@gmail.com>
2021-10-25 15:20:13 -0600

    Drawer added to nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SenatorContainer from './pages/SenatorContainer'
 import RepsContainer from './pages/RepsContainer'
 import { CongressContextProvider } from './contexts/CongressContext'
 import ButtonAppBar from './components/nav/ButtonAppBar'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import Welcome from './pages/Welcome'
 import Products from './pages/Products'
 import LoginForm from './components/login/LoginForm'
@@ -13,11 +13,24 @@ const App = () => {
     <div className='App'>
       <CongressContextProvider>
         <ButtonAppBar />
-        <Route path="/welcome"><Welcome/></Route>
-        <Route path="/senators"><SenatorContainer /></Route>
-        <Route path="/reps"><RepsContainer/></Route>
-        <Route path="/products"><Products/></Route>
-        <Route path="/login"><LoginForm/></Route>
+        <Route path='/' exact>
+          <Redirect to='/welcome' />
+        </Route>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/senators'>
+          <SenatorContainer />
+        </Route>
+        <Route path='/reps'>
+          <RepsContainer />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+        <Route path='/login'>
+          <LoginForm />
+        </Route>
       </CongressContextProvider>
     </div>
   )
--- src/components/nav/ButtonAppBar.js
+++ src/components/nav/ButtonAppBar.js
@@ -7,16 +7,43 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { useCongressContext } from '../../contexts/CongressContext'
-//import Modal from '@mui/material/Modal'
-//import LoginForm from '../login/LoginForm'
-import { NavLink } from 'react-router-dom'
-
+import { NavLink, useHistory } from 'react-router-dom'
+import { ListItem, List, Drawer, ListItemIcon, ListItemText } from '@mui/material'
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople'
+import GroupIcon from '@mui/icons-material/Group';
 
 const ButtonAppBar = () => {
   const memberData = useCongressContext()
-  // const [open, setOpen] = React.useState(false)
-  // const handleOpen = () => setOpen(true)
-  //const handleClose = () => setOpen(false)
+  const history = useHistory()
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleNavChoice = () => {
+    history.push('/senators')
+    toggleDrawer()
+  }
+
+  const drawerItemList = () => (
+    <Box sx={{ width: 250 }} role="presentation">
+      <List>
+        <ListItem button onClick={handleNavChoice}>
+          <ListItemIcon>
+            <EmojiPeopleIcon/>
+          </ListItemIcon>
+          <ListItemText primary='Senators' />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <GroupIcon/>
+          </ListItemIcon>
+          <ListItemText primary='Representatives'/>
+        </ListItem>
+      </List>
+    </Box>
+  )
 
   return (
     <>
@@ -29,6 +56,7 @@ const ButtonAppBar = () => {
               color='inherit'
               aria-label='menu'
               sx={{ mr: 2 }}
+              onClick={toggleDrawer}
             >
               <MenuIcon />
             </IconButton>
@@ -42,9 +70,9 @@ const ButtonAppBar = () => {
           </Toolbar>
         </AppBar>
       </Box>
-{/*       <Modal open={open}>
-              <LoginForm closeHandler={handleClose}/>
-      </Modal> */}
+      <Drawer anchor='left' open={isOpen} onClose={toggleDrawer}>
+        {drawerItemList()}
+      </Drawer>
     </>
   )
 }
