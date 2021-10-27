6a4e5a0b8f3140bce6151dc79cabbc27d8673454
Thor Anderson <ta.anderson@gmail.com>
2021-10-27 15:07:25 -0600

    sample table

---
+++ src/sampleTable.js
@@ -0,0 +1,76 @@
+<Table>
+  <TableBody>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          Home Page
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.url}
+        </Typography>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          Phone
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.phone}
+        </Typography>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          Party
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.party}
+        </Typography>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          State/Region
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.state}
+        </Typography>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          Office
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.office}
+        </Typography>
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>
+        <Typography color="textPrimary" variant="subtitle2">
+          Leadership Role
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Typography color="textSecondary" variant="body2">
+          {member.leadership_role}
+        </Typography>
+      </TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
