3d243980f33683a6f1ec5d9c95332bd82fccca07
Thor Anderson <ta.anderson@gmail.com>
2021-11-01 15:16:18 -0600

    Added Suspense and Lazy image loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
-import SenatorContainer from './pages/SenatorContainer'
-import RepsContainer from './pages/RepsContainer'
+import React, { Suspense } from 'react'
+//import SenatorContainer from './pages/SenatorContainer'
+//import RepsContainer from './pages/RepsContainer'
 import { CongressContextProvider } from './contexts/CongressContext'
 import ButtonAppBar from './components/nav/ButtonAppBar'
 import { Route, Switch, Redirect } from 'react-router-dom'
@@ -8,35 +9,54 @@ import Welcome from './pages/Welcome'
 import MemberDetail from './pages/MemberDetail'
 import LoginForm from './components/login/LoginForm'
 import NotFound from './pages/NotFound'
+import { Box, CircularProgress } from '@mui/material'
+
+const SenatorContainer = React.lazy(() => import('./pages/SenatorContainer'))
+const RepsContainer = React.lazy(() => import('./pages/RepsContainer'))
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+}
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <CongressContextProvider>
         <ButtonAppBar />
-        <Switch>
-        <Route path='/' exact>
-          <Redirect to='/welcome' />
-        </Route>
-        <Route path='/welcome'>
-          <Welcome />
-        </Route>
-        <Route path='/senators'>
-          <SenatorContainer />
-        </Route>
-        <Route path='/reps'>
-          <RepsContainer />
-        </Route>
-        <Route path='/member/:memberId' exact>
-          <MemberDetail />
-        </Route>
-        <Route path='/login'>
-          <LoginForm />
-          </Route>
-          <Route path='*'>
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <Box sx={style}>
+              <CircularProgress />
+            </Box>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/welcome" />
+            </Route>
+            <Route path="/welcome">
+              <Welcome />
+            </Route>
+            <Route path="/senators">
+              <SenatorContainer />
+            </Route>
+            <Route path="/reps">
+              <RepsContainer />
+            </Route>
+            <Route path="/member/:memberId" exact>
+              <MemberDetail />
+            </Route>
+            <Route path="/login">
+              <LoginForm />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </CongressContextProvider>
     </div>
   )
