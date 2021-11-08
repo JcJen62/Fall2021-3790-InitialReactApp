ed89a9ad840f520323454bbd1213a811ee7d2e5d
Thor Anderson <ta.anderson@gmail.com>
2021-11-08 09:02:34 -0700

    New auth context provider

---
+++ src/contexts/IdentityAuthContext.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import GoTrue from 'gotrue-js'
+
+const IdentityAuthContext = React.createContext({
+    user: {},
+    auth: {},
+    login: () => { },
+    logout: () => { },
+    signup: () => { },
+})
+
+export const IdentityAuthContextProvider = (props) => {
+    const [user, setUser] = React.useState({})
+    const [auth, setAuth] = React.useState({})
+
+    React.useEffect(() => {
+        setAuth(new GoTrue({
+            APIUrl: 'https://confident-yonath-6c5a52.netlify.app/.netlify/identity',
+            audience: '',
+            setCookie: false,
+        }))
+    }, [])
+
+    const login = (email, password, remember) => {
+        auth.login(email, password, remember)
+            .then((response) => {
+                console.log('Success logging in!', response)
+                setUser(auth.currentUser())
+            })
+        .catch((error) => console.log('Error: ', error))
+    }
+
+    const logout = () => {
+        return user.logout()
+    }
+
+    const signup = (email, password) => {
+        auth.signup(email, password)
+            .then((response) => console.log('Confirmation email sent', response))
+        .catch((error) => console.log("It's an error: ", error))
+    }
+
+
+return (
+    <IdentityAuthContext.Provider value={{
+        user,
+        login,
+        logout,
+        signup,
+    }}>{props.children}</IdentityAuthContext.Provider>
+)
+}
+
+export const useIdentityAuthContext = () => React.useContext(IdentityAuthContext)
