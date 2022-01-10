4038fc65e4ae4f8dc89624559d04b92291920010
jcjen62 <jcjensen62@gmail.com>
2022-01-09 20:11:26 -0700

    Testing

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -11,16 +11,16 @@ const AnimeCard = (props) => {
     const [added, setAdded] = useState(false)
     const { anime } = props
     const context = useAnimeContext()
-    const [setFavorite] = useState(false)
+    // const [setFavorite] = useState(false)
     const handleFave = (bool) => {
         setAdded(bool)
-        updateFavorites(anime)
+        // updateFavorites(anime)
     }
-    const { favorites, updateFavorites } = useAnimeContext()
+    // const { favorites, updateFavorites } = useAnimeContext()
 
-    React.useEffect(() => {
-        favorites.includes(anime.mal_id) ? setFavorite(true) : setFavorite(false)
-    }, [anime.mal_id, favorites, setFavorite])
+    // React.useEffect(() => {
+    //     favorites.includes(anime.mal_id) ? setFavorite(true) : setFavorite(false)
+    // }, [anime.mal_id, favorites, setFavorite])
 
     return (<Grid className="itemFlex" item xs={4} key={anime.mal_id}>
         <img src={anime.image_url} alt="Anime Poster" />
--- src/components/MangaCard.js
+++ src/components/MangaCard.js
@@ -11,16 +11,16 @@ const MangaCard = (props) => {
     const [added, setAdded] = useState(false)
     const {manga} = props
     const context = useAnimeContext()
-    const [setFavorite] = React.useState(false)
+    // const [setFavorite] = React.useState(false)
     const handleFave = (bool) => {
         setAdded(bool)
-        updateFavorites(manga)
+        // updateFavorites(manga)
     }
-    const { favorites, updateFavorites } = useAnimeContext()
+    // const { favorites, updateFavorites } = useAnimeContext()
 
-    React.useEffect(() => {
-        favorites.includes(manga.mal_id) ? setFavorite(true) : setFavorite(false)
-    }, [manga.mal_id, favorites, setFavorite])
+    // React.useEffect(() => {
+    //     favorites.includes(manga.mal_id) ? setFavorite(true) : setFavorite(false)
+    // }, [manga.mal_id, favorites, setFavorite])
 
     return (<Grid className="itemFlex" item xs={4} key={manga.mal_id}>
         <img src={manga.image_url} alt="Manga Poster" />
